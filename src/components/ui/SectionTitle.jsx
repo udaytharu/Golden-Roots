e3f2d3ace52c06af10b2acc89978c10fedde3b8b
Uday Tharu <160394594+udaytharu@users.noreply.github.com>
2026-09-19 11:44:44 +0000

    delete

--- src/components/ui/SectionTitle.jsx
+++
@@ -1,8 +0,0 @@
-export default function SectionTitle({ title, subtitle }) {
-  return (
-    <>
-      <h2 className="section-title">{title}</h2>
-      {subtitle && <p className="section-subtitle">{subtitle}</p>}
-    </>
-  );
-}
